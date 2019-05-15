7f96935c7b65b2238f1e03272f0d4faafe3169b9
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2019-05-15 11:32:58 +0300

    fix stack trace max length

--- src/main/java/com/mbi/api/entities/testrun/TestCaseEntity.java
+++ src/main/java/com/mbi/api/entities/testrun/TestCaseEntity.java
@@ -20,7 +20,7 @@ public class TestCaseEntity {
 
     private String status;
 
-    @Column(length = 10_000)
+    @Column(length = 15_000)
     private String exception;
 
     private String className;
