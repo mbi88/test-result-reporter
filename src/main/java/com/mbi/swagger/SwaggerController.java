fd643d7e659a3a3b6b8e434d1350b2a9b9890d6d
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2021-03-19 21:30:21 +0200

    fix swagger

--- src/main/java/com/mbi/swagger/SwaggerController.java
+++ src/main/java/com/mbi/swagger/SwaggerController.java
@@ -3,7 +3,6 @@ package com.mbi.swagger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
