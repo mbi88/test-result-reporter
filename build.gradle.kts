888794b2037b0ceaedf2492636cbc17ccb298aaa
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2021-12-15 17:51:48 +0200

    update depdendencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot").version("2.4.5")
+    id("org.springframework.boot").version("2.6.1")
     id("io.spring.dependency-management").version("1.0.11.RELEASE")
     id("java")
 }
@@ -21,14 +21,18 @@ dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-test")
     implementation("io.springfox", "springfox-boot-starter", "3.0.0")
     implementation("org.postgresql", "postgresql")
-    implementation("org.modelmapper", "modelmapper", "2.4.3")
-    implementation("javax.xml.bind", "jaxb-api", "2.3.1")
-    implementation("com.sun.xml.bind", "jaxb-impl", "2.3.1")
-    implementation("com.sun.xml.bind", "jaxb-core", "2.3.0.1")
+    implementation("org.modelmapper", "modelmapper", "2.4.5")
+    implementation("javax.xml.bind", "jaxb-api", "2.4.0-b180830.0359")
+    implementation("com.sun.xml.bind", "jaxb-impl", "3.0.2")
+    implementation("com.sun.xml.bind", "jaxb-core", "3.0.2")
     implementation("javax.activation", "activation", "1.1.1")
-    implementation("org.glassfish.jaxb", "jaxb-runtime", "3.0.1")
-    implementation("org.json", "json", "20210307")
-    implementation("com.vladmihalcea", "hibernate-types-52", "2.11.1")
+    implementation("org.glassfish.jaxb", "jaxb-runtime", "3.0.2")
+    implementation("org.json", "json", "20211205")
+    implementation("com.vladmihalcea", "hibernate-types-52", "2.14.0")
+}
+
+tasks.withType<Jar>().all {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 java {
